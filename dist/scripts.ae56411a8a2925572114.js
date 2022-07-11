e8d51d9956231782e121ed58d2d99abbcb080bc9
Julia V <vid090293@yandex.ru>
2022-07-11 09:25:07 +0300

    First commit

---
+++ dist/scripts.ae56411a8a2925572114.js
@@ -0,0 +1,2 @@
+!function(){"use strict";console.log({name:"Vitalij",age:"37",language:"JavaScript",framework:"Angular"})}();
+//# sourceMappingURL=scripts.ae56411a8a2925572114.js.map
---
+++ dist/user.496a834077033a95b16b.js
@@ -0,0 +1,2 @@
+console.log("JJJJJJ");
+//# sourceMappingURL=user.496a834077033a95b16b.js.map
